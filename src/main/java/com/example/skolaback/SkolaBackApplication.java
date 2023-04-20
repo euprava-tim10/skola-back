f2f099847a09a07f9a5aa2d554e3091f9f8a3b4c
Stefan Vlajkovic <vlajkovicstefan01@gmail.com>
2023-04-20 16:04:57 +0200

    Created spring boot project

---
+++ src/main/java/com/example/skolaback/SkolaBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.skolaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkolaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkolaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/skolaback/SkolaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skolaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkolaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
