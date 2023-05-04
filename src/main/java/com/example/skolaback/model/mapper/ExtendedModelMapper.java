a7c74001f8aa5747666944c2316092daac5449c0
borisavz <borisavzivanovic@gmail.com>
2023-05-04 13:48:29 +0200

    Java 11, 2 stage build, retry DB connection

--- src/main/java/com/example/skolaback/model/mapper/ExtendedModelMapper.java
+++ src/main/java/com/example/skolaback/model/mapper/ExtendedModelMapper.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class ExtendedModelMapper extends ModelMapper {
@@ -12,7 +13,7 @@ public class ExtendedModelMapper extends ModelMapper {
     public <S, T> List<T> mapAll(List<S> source, Class<T> targetClass) {
         return source
                 .stream()
-                .map(element -> map(element, targetClass)).toList();
+                .map(element -> map(element, targetClass)).collect(Collectors.toList());
     }
 
     public <S, T> Page<T> mapAll(Page<S> source, Class<T> targetClass) {
