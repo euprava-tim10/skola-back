3d7a94783d1797a8303d61eda8456a259455f9ad
borisavz <borisavzivanovic@gmail.com>
2023-06-01 18:02:17 +0200

    Recreate database on startup

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,29 +1,42 @@
-INSERT INTO school(name, place, type) VALUES ('Vuk Karadzic', 'Loznica', 'OSNOVNA');
-INSERT INTO school(name, place, type) VALUES ('Nikola Tesla', 'Novi Sad', 'SREDNJA');
+UPDATE diploma
+SET student_id = NULL;
 
-INSERT INTO course(name, school_id) VALUES ('Informacione Tehnologije', 2);
-INSERT INTO course(name, school_id) VALUES ('Masinski tehnicar',  2);
+UPDATE student
+SET school_id = NULL;
 
-INSERT INTO administrator(first_name, last_name, jmbg, school_id) VALUES ('Stefan', 'Vlajkovic', '1234567891234', 1);
-INSERT INTO administrator(first_name, last_name, jmbg, school_id) VALUES ('Nikola', 'Nikolic', '9876543214321', 2);
+DELETE FROM diploma_gpa_mapping;
+DELETE FROM student_gpa_mapping;
 
-INSERT INTO student(first_name, last_name, jmbg, school_id, course) VALUES ('Petar', 'Petrovic', '123412341234', 1, null);
+DELETE FROM skola.administrator;
+DELETE FROM skola.contest;
+DELETE FROM skola.contest_application;
+DELETE FROM skola.contest_quotas;
+DELETE FROM skola.course;
+DELETE FROM skola.course_quota;
+DELETE FROM skola.diploma;
+DELETE FROM skola.school;
+DELETE FROM skola.student;
+
+
+INSERT INTO school(id, name, place, type) VALUES (1, 'Vuk Karadzic', 'Loznica', 'OSNOVNA');
+INSERT INTO school(id, name, place, type) VALUES (2, 'Nikola Tesla', 'Novi Sad', 'SREDNJA');
+
+INSERT INTO course(id, name, school_id) VALUES (1, 'Informacione Tehnologije', 2);
+INSERT INTO course(id, name, school_id) VALUES (2, 'Masinski tehnicar',  2);
+
+INSERT INTO administrator(id, first_name, last_name, jmbg, school_id) VALUES (1, 'Stefan', 'Vlajkovic', '1234567891234', 1);
+INSERT INTO administrator(id, first_name, last_name, jmbg, school_id) VALUES (2, 'Nikola', 'Nikolic', '9876543214321', 2);
+
+INSERT INTO student(id, first_name, last_name, jmbg, school_id, course) VALUES (1, 'Petar', 'Petrovic', '123412341234', 1, null);
+
 INSERT INTO student_gpa_mapping(student_id, gpa, grade_year) VALUES (1, 4.50, 5);
 INSERT INTO student_gpa_mapping(student_id, gpa, grade_year) VALUES (1, 4.50, 6);
 INSERT INTO student_gpa_mapping(student_id, gpa, grade_year) VALUES (1, 4.50, 7);
 INSERT INTO student_gpa_mapping(student_id, gpa, grade_year) VALUES (1, 4.50, 8);
 
-INSERT INTO diploma(course, profession, date, school_id, student_id) VALUES (null, 'OTHER', '2022-07-01', 1, 1);
+INSERT INTO diploma(id, course, profession, date, school_id, student_id) VALUES (1, null, 'OTHER', '2022-07-01', 1, 1);
+
 INSERT INTO diploma_gpa_mapping(diploma_id, gpa, grade_year) VALUES (1, 4.50, 5);
 INSERT INTO diploma_gpa_mapping(diploma_id, gpa, grade_year) VALUES (1, 4.50, 6);
 INSERT INTO diploma_gpa_mapping(diploma_id, gpa, grade_year) VALUES (1, 4.50, 7);
 INSERT INTO diploma_gpa_mapping(diploma_id, gpa, grade_year) VALUES (1, 4.50, 8);
-
-
-SELECT * FROM contest;
-SELECT * FROM contest_quotas;
-SELECT * FROM course_quota;
-SELECT * FROM course;
-SELECT * FROM diploma;
-SELECT * FROM contest_application;
-SELECT * FROM student;
